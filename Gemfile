8f253604866d2ae139445c08b2f6674c6c60e5f9
Jacob Angel <jacob.angel@gmail.com>
2013-04-05 18:19:01 -0400

    Reorganizing files, adding more style and content to various pages.
    Update the gemfile as well...

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # Gemfile
 source "https://rubygems.org"
 
+gem 'rack'
 gem 'sinatra', '1.4.2'
 gem 'sass'
 gem 'bootstrap-sass',  '~> 2.3.1.0'
--- app.rb
+++ app.rb
@@ -12,38 +12,38 @@ BASE_TITLE = "Sandy & Jacob's Wedding"
 # Routes
 get '/' do
 	@title = "Jake and Sandy the Marriaging | #{BASE_TITLE}"
-    @body_class = "page-home"
+	@body_class = "page-home"
 	erb :index, :layout => :splash
 end
 
 get '/prologue' do
 	@title = "That Which Came Before | #{BASE_TITLE}"
-    @body_class = "page-prologue"
+	@body_class = "page-prologue"
 	erb :prologue
 end
 
 get '/details' do
-    @title = "The Deets | #{BASE_TITLE}"
-    @body_class = "page-details"
-    erb :details
+	@title = "The Deets | #{BASE_TITLE}"
+	@body_class = "page-details"
+	erb :details
 end
 
 get '/who' do
-    @title = "Major Players | #{BASE_TITLE}"
-    @body_class = "page-who"
-    erb :who
+	@title = "Major Players | #{BASE_TITLE}"
+	@body_class = "page-who"
+	erb :who
 end
 
 get '/registry' do
-    @title = "Lavish Us with Gifts | #{BASE_TITLE}"
-    @body_class = "page-registry"
-    erb :registry
+	@title = "Lavish Us with Gifts | #{BASE_TITLE}"
+	@body_class = "page-registry"
+	erb :registry
 end
 
 get '/rsvp' do
-    @title = "Répondez s'il vous plaît | #{BASE_TITLE}"
-    @body_class = "page-rsvp"
-    erb :rsvp
+	@title = "Répondez s'il vous plaît | #{BASE_TITLE}"
+	@body_class = "page-rsvp"
+	erb :rsvp
 end
 
 helpers do
@@ -54,7 +54,7 @@ helpers do
 			super
 		end
 	end
-    def find_template(views, name, engine, &block)
-        Array(views).each { |v| super(v, name, engine, &block) }
-    end
+	def find_template(views, name, engine, &block)
+		Array(views).each { |v| super(v, name, engine, &block) }
+	end
 end
