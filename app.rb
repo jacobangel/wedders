df51d3de69b06767f663af56213f4b4de691abe6
Jacob Angel <jacob.angel@gmail.com>
2013-04-05 01:30:19 -0400

    Adding the homepage.

--- app.rb
+++ app.rb
@@ -2,11 +2,26 @@
 # Basically a super small app
 require 'sinatra'
 
+
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 # Routes
 get '/' do
-	erb :index
+	@title = "Jake and Sandy the Marriaging"
+	erb :index, :layout => :splash
 end
 
+get '/about' do
+	@title = "About Sandy & Jacob's Wedding"
+	erb :about
+end
 
+helpers do
+	def render(*args)
+		if args.first.is_a?(Hash) && args.first.keys.include?(:partial)
+			return erb "#{args.first[:partial]}".to_sym, :layout => false
+		else
+			super
+		end
+	end
+end
---
+++ public/scripts/site.js
@@ -0,0 +1,8 @@
+/**
+ * Site script for jakeandsandy.us
+ */
+!(function($) {
+	$(function() {
+		  $(".row").find("h1").fitText(1, { minFontSize: '15px', maxFontSize: '200px' });
+	});
+}(jQuery))
