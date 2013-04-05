fed556350addb95c116660fee35a8027f93a83ea
Jacob Angel <jacob.angel@gmail.com>
2013-04-05 16:55:58 -0400

    Reorganizing sass and views.

--- app.rb
+++ app.rb
@@ -5,27 +5,32 @@ require 'sinatra'
 
 
 set :public_folder, File.dirname(__FILE__) + '/public'
+set :views, ['views/partials', 'views/pages', 'views/layouts', 'views']
 
 BASE_TITLE = "Sandy & Jacob's Wedding"
 
 # Routes
 get '/' do
 	@title = "Jake and Sandy the Marriaging | #{BASE_TITLE}"
+    @body_class = "page-home"
 	erb :index, :layout => :splash
 end
 
 get '/about' do
 	@title = "About | #{BASE_TITLE}"
+    @body_class = "page-about"
 	erb :about
 end
 
 get '/details' do
     @title = "The Deets | #{BASE_TITLE}"
+    @body_class = "page-details"
     erb :details
 end
 
 get '/who' do
     @title = "Major Players | #{BASE_TITLE}"
+    @body_class = "page-who"
     erb :who
 end
 
@@ -38,4 +43,7 @@ helpers do
 			super
 		end
 	end
+    def find_template(views, name, engine, &block)
+        Array(views).each { |v| super(v, name, engine, &block) }
+    end
 end
