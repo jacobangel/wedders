24dba16a99f4709806e7fc65efa55629c342f533
Jacob Angel <jacob.angel@gmail.com>
2013-04-05 22:48:21 -0400

    Further articulating the various pages.

--- config.rb
+++ config.rb
@@ -8,6 +8,9 @@ sass_dir = "sass"
 images_dir = "public/images"
 javascripts_dir = "public/scripts"
 
+http_path = "/"
+http_images_path = "/images"
+http_stylesheets_path = "/css"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
